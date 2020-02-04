e34cb00564542c505ae9b25691e724d40315b773
Giuseppe SARDA <gsarda@lopo2.ecslab.tuwien.ac.at>
2020-02-04 11:14:50 +0100

    report of quantized vgg bn performance obtained

--- scripts/dataGet_single_postTrain.py
+++ scripts/dataGet_single_postTrain.py
@@ -67,8 +67,8 @@ data_loader= torch.utils.data.DataLoader(
     ,batch_size=batch_size)
 
 mask_config_file="../models/mask_config/{}.mc".format(network_name)
-guided_MaskTable_creator(network, mask_config_file,std_mask="00000100")
-mask_table=MaskTable(distiller.quantization.LinearQuantMode.ASYMMETRIC_UNSIGNED, MaskType.MOD_ROUND_UP, [], False, network, mask_file=mask_config_file)
+guided_MaskTable_creator(network, mask_config_file,std_mask="00001000")
+mask_table=MaskTable(distiller.quantization.LinearQuantMode.ASYMMETRIC_UNSIGNED, MaskType.MD, [], False, network, mask_file=mask_config_file)
 test_preds = get_all_preds(network, data_loader,device=device)
 ref_correct = test_preds.argmax(dim=1).eq(torch.LongTensor(train_set.targets)).sum().item()
 print(ref_correct)
